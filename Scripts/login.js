dcc17013b33a57e9ba9873efaaab650a820ee579
Tiaan de Beer <tdebeer.za@gmail.com>
2022-08-23 14:55:17 +0200

    Updates to basic demo

---
+++ Scripts/login.js
@@ -0,0 +1,41 @@
+let users = JSON.parse(localStorage.getItem("users"));
+let message = document.getElementById("msg");
+
+function signUp() {
+  window.location.href = "../Pages/signup-page.html";
+}
+
+document.getElementById("login-form").addEventListener("submit", function (e) {
+  let username = document.getElementById("user").value;
+  let password = document.getElementById("pass").value;
+  let isUser = false;
+  let id = 0;
+
+  for (let i = 0; i < users.length; i++) {
+    if (username == users[i].username && password == users[i].password) {
+      isUser = true;
+      id = i;
+    }
+  }
+
+  console.log(isUser);
+
+  if (!isUser) {
+    message.style.color = "red";
+    message.innerText = "Invalid username or password";
+    username.innerText = "";
+    password.innerText = "";
+    e.preventDefault();
+  } else {
+    e.preventDefault();
+    alert("Welcome: " + users[id].username);
+    localStorage.setItem("currentUser", users[id].username);
+    window.location.href = "../Pages/task-page.html";
+  }
+});
+
+function createNewUser() {
+  let username = document.getElementById("user").value;
+  let password = document.getElementById("pass").value;
+  let confirmPassword = document.getElementById("conf-pass").value;
+}
---
+++ Scripts/main.js
@@ -0,0 +1,23 @@
+class User {
+  constructor(firstname, lastname, username, password, auth) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.username = username;
+    this.password = password;
+    this.auth = auth;
+  }
+}
+
+let users = [];
+//Loads dummy data into localstorage
+window.onload = function () {
+  //dummy users section
+  let user = new User("Administrator", "Profile", "Admin", "1234", "admin");
+  users.push(user);
+  user = new User("Tiaan", "De Beer", "T-deBeer", "1007", "developer");
+  users.push(user);
+  localStorage.setItem("users", JSON.stringify(users));
+  //dummy users section ends
+
+  console.log(localStorage.getItem("users"));
+};
