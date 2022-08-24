856499317f5639dd65042827ae4075b93348be78
Jaco <sjacomathee@gmail.com>
2022-08-24 14:23:56 +0200

    Progressbar script

--- Scripts/task-page.js
+++ Scripts/task-page.js
@@ -145,3 +145,30 @@ document.getElementById("edit-form").addEventListener("submit", function () {
     }
   }
 });
+
+function loadprogressBar(states = ['Unresolved', 'Proccessing', 'Resolved', 'Proccessing', 'Resolved']){
+  let u = document.getElementById('progressUnresolved');
+  let p = document.getElementById('progressProccessing');
+  let r = document.getElementById('progressResolved');
+
+  let num1 = 0, num2 = 0, num3 = 0;
+
+  states.forEach(element => {
+    if(element === 'Unresolved')
+    {
+        num1+= ((1/tickets.length) * 240)/1
+    }
+    if(element === 'Proccessing')
+    {
+        num2+= ((1/tickets.length) * 240)/1
+    }
+    if(element === 'Resolved')
+    {
+        num3+= ((1/tickets.length) * 240)/1
+    }
+  });
+
+  u.style.width = `${num1}px`;
+  p.style.width = `${num2}px`;
+  r.style.width = `${num3}px`;
+}
