97274ac5e37aa90705faa8c9832c670e90957f68
Jaco <sjacomathee@gmail.com>
2022-08-25 12:26:28 +0200

    Updated Overview To Progress Bar

--- Scripts/task-page.js
+++ Scripts/task-page.js
@@ -284,3 +284,75 @@ function featureReq() {
     );
   }
 }
+
+function loadprogressBar(){
+  let u = document.getElementById('progressUnresolved');
+  let p = document.getElementById('progressProccessing');
+  let r = document.getElementById('progressResolved');
+
+  let states = ['Proccessing','Unresolved', 'Proccessing', 'Resolved', 'Resolved', 'Resolved'];
+
+  let num1 = 0, num2 = 0, num3 = 0;
+
+  states.forEach(element => {
+      if(element === 'Unresolved')
+      {
+          num1+= ((1/states.length) * 240)/1
+      }
+      if(element === 'Proccessing')
+      {
+          num2+= ((1/states.length) * 240)/1
+      }
+      if(element === 'Resolved')
+      {
+          num3+= ((1/states.length) * 240)/1
+      }
+  });
+  u.style.width = `${num1}px`;
+  p.style.width = `${num2}px`;
+  r.style.width = `${num3}px`;
+}
+
+function showDetails()
+{
+  let progressbar = document.getElementById('progressbar');
+  let u = document.getElementById('progressUnresolved');
+  let p = document.getElementById('progressProccessing');
+  let r = document.getElementById('progressResolved');
+  u.style.opacity = '0.2';
+  p.style.opacity = '0.2';
+  r.style.opacity = '0.2';
+  progressbar.style.height = "500px";
+  let menue = document.getElementById('menue');
+  menue.style.display = "block";
+}
+
+function hideDetails()
+{
+  let progressbar = document.getElementById('progressbar');
+  progressbar.style.height = "20px";
+  let u = document.getElementById('progressUnresolved');
+  let p = document.getElementById('progressProccessing');
+  let r = document.getElementById('progressResolved');
+  let menue = document.getElementById('menue');
+  menue.style.display = "none";
+  u.style.opacity = '1';
+  p.style.opacity = '1';
+  r.style.opacity = '1';
+  
+  hideInfo();
+}
+
+function showInfo(){
+  let paragraph = document.getElementById('paragraph');
+  paragraph.style.display = 'block';
+  let fallAway = document.getElementById('fallAway');
+  fallAway.style.display ='none'
+}
+
+function hideInfo(){
+  let paragraph = document.getElementById('paragraph');
+  paragraph.style.display = 'none'
+  let fallAway = document.getElementById('fallAway');
+  fallAway.style.display ='block';
+}
