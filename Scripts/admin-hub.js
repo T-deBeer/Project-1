d77e7013ed5d9a718fce43187365630de5dc6807
Tiaan de Beer <tdebeer.za@gmail.com>
2022-08-29 12:31:24 +0200

    Slight Style changes

--- Scripts/admin-hub.js
+++ Scripts/admin-hub.js
@@ -8,63 +8,65 @@ class DevUser {
   }
 }
 
-
 //Opens up the add dev modal
-document.getElementById("Add-DEV-button").addEventListener("click", function () {
-  let modal = document.getElementById("AddDev-modal");
+document
+  .getElementById("Add-DEV-button")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("AddDev-modal");
 
-  modal.style.display = "block";
-});
+    modal.style.display = "block";
+  });
 
 //Opens up the dev Reset warning modal
-document.getElementById("Reset-DEV-button").addEventListener("click", function () {
-  let modal = document.getElementById("ResetDEV-modal");
+document
+  .getElementById("Reset-DEV-button")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("ResetDEV-modal");
 
-  modal.style.display = "block";
-});
+    modal.style.display = "block";
+  });
 
 //Opens up the remove dev modal
-document.getElementById("Remove-DEV-button").addEventListener("click", function () {
-
-  let modal = document.getElementById("RemoveDEV-modal");
-  modal.style.display = "block";  
-
-});
-
-
-
+document
+  .getElementById("Remove-DEV-button")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("RemoveDEV-modal");
+    modal.style.display = "block";
+  });
 
 //When remove button is clicked in the remove modal(Remove from localstorage)
-document.getElementById("Remove-DEVform").addEventListener("submit", function () {
-  let RemoveDevUsername = document.getElementById("Remove-DEV").value;
+document
+  .getElementById("Remove-DEVform")
+  .addEventListener("submit", function () {
+    let RemoveDevUsername = document.getElementById("Remove-DEV").value;
 
-  let users = [];
-  let newUsers = [];''
-  users = JSON.parse(localStorage.getItem("users") || "[]");
-  len = users.length;
+    let users = [];
+    let newUsers = [];
+    ("");
+    users = JSON.parse(localStorage.getItem("users") || "[]");
+    len = users.length;
 
-  for(let i=0; i < len; i++)
-  {
-    if((users[i].username != RemoveDevUsername) && (len != 0))
-    {
-      let DevName = users[i].firstname;
-      let DevSurname = users[i].lastname;
-      let DevUsername = users[i].username;
-      let DevPassword = users[i].password;
-      let DevAuth = users[i].auth;
-      let user = new DevUser(DevName, DevSurname, DevUsername, DevPassword, DevAuth);
-      newUsers.push(user);
-
+    for (let i = 0; i < len; i++) {
+      if (users[i].username != RemoveDevUsername && len != 0) {
+        let DevName = users[i].firstname;
+        let DevSurname = users[i].lastname;
+        let DevUsername = users[i].username;
+        let DevPassword = users[i].password;
+        let DevAuth = users[i].auth;
+        let user = new DevUser(
+          DevName,
+          DevSurname,
+          DevUsername,
+          DevPassword,
+          DevAuth
+        );
+        newUsers.push(user);
+      }
     }
-    
-    
-  }
-  
-  localStorage.clear("users");
-  localStorage.setItem("users", JSON.stringify(newUsers));
 
-});
-
+    localStorage.clear("users");
+    localStorage.setItem("users", JSON.stringify(newUsers));
+  });
 
 //Add developer to local storage(Button inside add modal)
 document.getElementById("edit-DEVform").addEventListener("submit", function () {
@@ -76,55 +78,80 @@ document.getElementById("edit-DEVform").addEventListener("submit", function () {
   let DevUsername = document.getElementById("DEV-Add-Username").value;
   let DevPassword = document.getElementById("DEV-Add-Password").value;
 
-  let user = new DevUser(DevName, DevSurname, DevUsername, DevPassword, "developer");
-  users.push(user); 
+  let user = new DevUser(
+    DevName,
+    DevSurname,
+    DevUsername,
+    DevPassword,
+    "developer"
+  );
+  users.push(user);
 
   localStorage.setItem("users", JSON.stringify(users));
-
 });
 
 //When reset button is clicked in reset modal
-document.getElementById("Reset-DEVform").addEventListener("submit", function () {
+document
+  .getElementById("Reset-DEVform")
+  .addEventListener("submit", function () {
+    localStorage.clear("users");
 
-  localStorage.clear("users");
+    users = JSON.parse(localStorage.getItem("users") || "[]");
+    let user = new DevUser(
+      "Administrator",
+      "Profile",
+      "Admin",
+      "1234",
+      "admin"
+    );
+    users.push(user);
+    user = new DevUser("Tiaan", "De Beer", "T-deBeer", "577088", "developer");
+    users.push(user);
+    user = new DevUser(
+      "Hardus",
+      "Lotter",
+      "HardusLotter",
+      "578559",
+      "developer"
+    );
+    users.push(user);
+    user = new DevUser("Jaco", "Mathee", "timejunky1", "578381", "developer");
+    users.push(user);
+    user = new DevUser(
+      "Tobias",
+      "van Schalkwyk",
+      "Rat3l",
+      "578552",
+      "developer"
+    );
+    users.push(user);
 
-  users = JSON.parse(localStorage.getItem("users") || "[]");
-  let user = new DevUser("Administrator", "Profile", "Admin", "1234", "admin");
-  users.push(user);
-  user = new DevUser("Tiaan", "De Beer", "T-deBeer", "577088", "developer");
-  users.push(user);
-  user = new DevUser("Hardus", "Lotter", "HardusLotter", "578559", "developer");
-  users.push(user);
-  user = new DevUser("Jaco", "Mathee", "timejunky1", "578381", "developer");
-  users.push(user);
-  user = new DevUser("Tobias", "van Schalkwyk", "Rat3l", "578552", "developer");
-  users.push(user);
-
-  localStorage.setItem("users", JSON.stringify(users));
-
-});
+    localStorage.setItem("users", JSON.stringify(users));
+  });
 
 //Close the add dev modal
-document.getElementById("closeDEV-modal").addEventListener("click", function () {
-  let modal = document.getElementById("AddDev-modal");
-  modal.style.display = "none";
-});
+document
+  .getElementById("closeDEV-modal")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("AddDev-modal");
+    modal.style.display = "none";
+  });
 
 //Close the remove dev modal
-document.getElementById("closeRemoveDEV-modal").addEventListener("click", function () {
-  let modal = document.getElementById("RemoveDEV-modal");
-  modal.style.display = "none";
-});
+document
+  .getElementById("closeRemoveDEV-modal")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("RemoveDEV-modal");
+    modal.style.display = "none";
+  });
 
 //Close the reset dev modal
-document.getElementById("closeResetDEV-modal").addEventListener("click", function () {
-  let modal = document.getElementById("ResetDEV-modal");
-  modal.style.display = "none";
-});
-
-
-
-
+document
+  .getElementById("closeResetDEV-modal")
+  .addEventListener("click", function () {
+    let modal = document.getElementById("ResetDEV-modal");
+    modal.style.display = "none";
+  });
 
 function logOut() {
   if (confirm("Are you sure you want to log out?")) {
@@ -767,8 +794,19 @@ document.getElementById("list-of-devs").addEventListener("change", function () {
 
 window.addEventListener("click", function (e) {
   let projModal = document.getElementById("project-modal");
-  if (e.target == projModal) {
+  let devModal = document.getElementById("AddDev-modal");
+  let resetModal = document.getElementById("ResetDEV-modal");
+  let removeModal = document.getElementById("RemoveDEV-modal");
+  if (
+    e.target == projModal ||
+    e.target == resetModal ||
+    e.target == removeModal ||
+    e.target == devModal
+  ) {
     projModal.style.display = "none";
+    devModal.style.display = "none";
+    resetModal.style.display = "none";
+    removeModal.style.display = "none";
   }
 });
 
